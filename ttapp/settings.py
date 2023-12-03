b46b4bcf71216507c59d745971252fe7c8e7ffa7
Vignesh Goswami <88871046+Vignesh16879@users.noreply.github.com>
2023-12-03 19:58:29 +0530

    Update settings.py

--- ttapp/settings.py
+++ ttapp/settings.py
@@ -112,10 +112,11 @@ WSGI_APPLICATION = 'ttapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'Vignesh9870',
+        'HOST': 'localhost',
+        'PORT': '5432',
     },
     'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
